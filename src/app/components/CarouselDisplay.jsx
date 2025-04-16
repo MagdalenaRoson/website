4a6df5d40cb4e9650ea78d306758c6741c43762c
yoricktf <yorick.tenfeld@gmail.com>
2025-04-16 13:44:14 +0200

    added font, cursor images and removed the hover overview when giong between images

---
+++ src/app/components/CarouselDisplay.jsx
@@ -0,0 +1,47 @@
+'use client';
+
+import { Carousel } from 'react-responsive-carousel';
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
+import Image from 'next/image';
+import contentfulLoader from '@/app/components/contentfulImageLoader';
+
+export default function CarouselDisplay({ items }) {
+  return (
+    <section className='carousel'>
+      <Carousel
+        showThumbs={false}
+        showStatus={false}
+        showIndicators={false}
+        infiniteLoop
+        useKeyboardArrows
+        dynamicHeight
+        emulateTouch
+      >
+        {items.map((post) => (
+          <>
+            <div className='contentWindow' key={post.sys.id}>
+              <Image
+                key={post.sys.id}
+                className='carouselImage'
+                loader={contentfulLoader}
+                src={`https:${post.fields.media.fields.file.url}`}
+                alt='media'
+                width={1600}
+                height={900}
+                style={{
+                  maxWidth: '100%',
+                  maxHeight: '100%',
+                  width: 'auto',
+                  height: 'auto',
+                }}
+              />
+              {/* <p className='legend'>
+                {post.fields.companyName} {post.fields.details}
+              </p> */}
+            </div>
+          </>
+        ))}
+      </Carousel>
+    </section>
+  );
+}
--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,8 @@
 import Image from 'next/image';
+import Link from 'next/link';
 import client from './lib/contenful';
 import { fetchEntries } from '@/app/lib/contenful';
-import { Carousel } from 'react-responsive-carousel';
-import 'react-responsive-carousel/lib/styles/carousel.min.css';
+import CarouselDisplay from '@/app/components/CarouselDisplay';
 
 export default async function Home() {
   const assets = await fetchEntries('asset'); // your content type ID
@@ -10,37 +10,12 @@ export default async function Home() {
   console.log('============*******88================', assets);
 
   return (
-    <div>
-      <h1>Blog</h1>
-      <Carousel
-        showThumbs={false}
-        showStatus={false}
-        infiniteLoop
-        useKeyboardArrows
-        dynamicHeight
-        emulateTouch
-      >
-        {assets.map((post) => (
-          <div key={post.sys.id}>
-            <div
-              style={{
-                position: 'relative',
-                width: '100%',
-                aspectRatio: '16 / 9',
-              }}
-            >
-              <Image
-                src={`https:${post.fields.media.fields.file.url}`}
-                alt='test'
-                fill
-                style={{ objectFit: 'cover' }}
-              />
-            </div>
-            <h2>{post.fields.companyName}</h2>
-            <p>{post.fields.details}</p>
-          </div>
-        ))}
-      </Carousel>
-    </div>
+    <>
+      <nav>
+        <Link href={'/'}>Home</Link>
+        <Link href={'/information'}>Information</Link>
+      </nav>
+      <CarouselDisplay items={assets} />
+    </>
   );
 }

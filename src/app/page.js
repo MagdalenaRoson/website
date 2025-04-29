e28fdbd4b52b2e7a5b6691d8c4741c0744b998cc
yoricktf <yorick.tenfeld@gmail.com>
2025-04-29 08:56:49 +0200

    removed console logs

--- src/app/page.js
+++ src/app/page.js
@@ -4,18 +4,6 @@ import CarouselDisplay from '@/app/components/CarouselDisplay';
 
 export default async function Home() {
   const assets = await fetchEntries('asset'); // your content type ID
-
-  console.log(`Well, well, well...
-Looks like you've found the hidden section.
-
-I'm a full-stack developer with a background in psychology and philosophy.
-Also a drummer, a cook, and a dog lover.
-
-Reach out:
-your.email@example.com
-github.com/yourusername
-linkedin.com/in/yourusername`);
-
   return (
     <>
       <Navbar />

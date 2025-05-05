11018758e5e16dc2ca2be94a2d29d1c6f9a2d649
yoricktf <yorick.tenfeld@gmail.com>
2025-05-05 15:08:07 +0200

    updated the loading component

---
+++ src/app/components/loadingcomponent.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Loadingcomponent = () => {
+  return (
+    <div className='loading'>
+      <h1>MAGDALENA ROSON</h1>
+    </div>
+  );
+};
+
+export default Loadingcomponent;

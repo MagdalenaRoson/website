621fb1f713a5b1f8b56b2a3e46ea31ef413a23f1
yoricktf <yorick.tenfeld@gmail.com>
2025-05-22 23:01:18 +0200

    changing the font color depending on section in viewport.

--- src/app/components/navbar.jsx
+++ src/app/components/navbar.jsx
@@ -1,12 +1,29 @@
-import React from 'react';
+'use client';
 import Link from 'next/link';
+import { useState } from 'react';
 
 const Navbar = () => {
+  const [activeSection, setActiveSection] = useState('projects');
+
   return (
     <nav>
-      <Link href={'#projects'}>PROJECTS</Link>
+      <a
+        onClick={() => setActiveSection('projects')}
+        href='#projects'
+        className={`nav-link${activeSection === 'projects' ? ' active' : ''}`}
+      >
+        PROJECTS
+      </a>
       <h1 className='navbarTitle'>Magdalena Roson</h1>
-      <Link href={'#information'}>INFO</Link>
+      <a
+        onClick={() => setActiveSection('information')}
+        href='#information'
+        className={`nav-link${
+          activeSection === 'information' ? ' active' : ''
+        }`}
+      >
+        INFO
+      </a>
     </nav>
   );
 };
